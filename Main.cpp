0fd405380230a2f312f30a9c7105c9ef9d68f10c
soleil <mprevot@freebsd.org>
2019-11-03 03:46:04 +0100

    TargetUpdate

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,9 @@
 #include "stdafx.h"
-#include "simpleD3D12.h"
+#include "simpleD3D12update.h"
 
 _Use_decl_annotations_
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
-	DX12CudaInterop sample(2160, 1080, "D3D12 CUDA Interop");
+	DX12CudaInterop sample(2160, 1080, "D3D12-CUDA Interop");
 	return Win32Application::Run(&sample, hInstance, nCmdShow);
 }
